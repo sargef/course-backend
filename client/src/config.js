904af6f0598610862340b5dd617424184db79c9b
Sarah Shelley <35126840+sargef@users.noreply.github.com>
2020-04-22 11:35:49 +0930

    client commit

---
+++ client/src/config.js
@@ -0,0 +1,3 @@
+export default {
+  apiBaseUrl: 'https://sleepy-beyond-18351.herokuapp.com/api'
+};
---
+++ client/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles/global.css';
+
+import { Provider } from  './Context';
+import App from './App';
+
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <Provider>
+  <App />
+  </Provider>,
+  document.getElementById('root'));
+
+serviceWorker.unregister();
